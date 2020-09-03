4e37042e3344873a5b28bbdeac03ba203471533b
cjoleary <cjoleary17@gmail.com>
2020-09-03 13:55:21 -0400

    created initial variables and phrases array

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 
+// Variables
+const keyBoard = document.querySelector( '#qwerty' );
+const phrase = document.querySelector( '#phrase' );
+let missed = 0;
+const phrases = [ 'basketball', 'baseball', 'football', 'hockey', 'soccer' ];
+
+
 // Hide start screen overlay
 const startBtn = document.querySelector( '.btn__reset' );
 const overlay = document.getElementById( 'overlay' );
-console.log(startBtn);
-console.log(overlay);
 startBtn.addEventListener( 'click', (e) => {
     e.preventDefault();
     overlay.style.display = 'none';
