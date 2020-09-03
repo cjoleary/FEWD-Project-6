5ca7da604cfe382e23f7434f62de95bed6ca94e8
cjoleary <cjoleary17@gmail.com>
2020-09-03 15:26:25 -0400

    Added function that loops through the array of characters, creates list items for them, and appends them to the #phrase ul

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
 
-// Variables
-const keyBoard = document.querySelector( '#qwerty' );
-const phrase = document.querySelector( '#phrase' );
-let missed = 0;
-const phrases = [ 'basketball', 'baseball', 'football', 'hockey', 'soccer' ];
-
-
 // Hide start screen overlay
 const startBtn = document.querySelector( '.btn__reset' );
 const overlay = document.getElementById( 'overlay' );
@@ -13,3 +6,39 @@ startBtn.addEventListener( 'click', (e) => {
     e.preventDefault();
     overlay.style.display = 'none';
 });
+
+// Variables
+const keyBoard = document.querySelector( '#qwerty' );
+const phraseUL = document.querySelector( '#phrase' );
+let missed = 0;
+const phrases = [
+    'A BLESSING IN DISGUISE',
+    'A DIME A DOZEN',
+    'ONCE IN A BLUE MOON', 
+    'EVERY DOG HAS HIS DAY', 
+    'IT TAKES TWO TO TANGO'
+];
+
+// Randomly chooses phrase from phrases array, returns a new array of characters from the randomly chosen phrase
+function getRandomPhraseAsArray ( arr ) {
+    let randomPhrase = arr[ Math.floor( Math.random() * arr.length ) ];
+    let phraseArray = randomPhrase.split('');
+    return phraseArray;
+}
+
+phraseArray = getRandomPhraseAsArray( phrases );
+
+// Loops through array of characters and appends them to the #phrase ul
+function addPhraseToDisplay( arr ) {
+    for ( let i = 0; i < arr.length; i++ ) {
+        const listItem = document.createElement('li');
+        listItem.textContent = arr[i];
+        console.log(listItem);
+        if ( listItem.textContent !== ' ' ) {
+            listItem.className = 'letter';
+        }
+        phraseUL.appendChild(listItem);
+    }
+}
+
+addPhraseToDisplay(phraseArray);
