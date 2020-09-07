0104141025d8ab6ef6f68f1bc962463aa0477d6e
cjoleary <cjoleary17@gmail.com>
2020-09-07 11:59:07 -0400

    Added reset game functions

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 
 // Global Variables
 const keyBoard = document.querySelector( '#qwerty' );
-const phraseUL = document.querySelector( '#phrase' );
+const phraseUL = document.querySelector( '#phrase' ).firstElementChild;
 const startBtn = document.querySelector( '.btn__reset' );
 const overlay = document.getElementById( 'overlay' );
-const title = document.querySelector( '.title' );
+const subTitle = document.querySelector( '.subTitle' );
 const scoreboard = document.querySelector('#scoreboard')
+const hearts = document.querySelectorAll('.tries')
 let missed = 0;
 const phrases = [
     'A BLESSING IN DISGUISE',
@@ -14,12 +15,6 @@ const phrases = [
     'EVERY DOG HAS HIS DAY', 
     'IT TAKES TWO TO TANGO'
 ];
-
-// Listen for the start game button to be pressed
-startBtn.addEventListener( 'click', (e) => {
-    e.preventDefault();
-    overlay.style.display = 'none';
-});
 
 // Randomly chooses phrase from phrases array, returns a new array of characters from the randomly chosen phrase
 function getRandomPhraseAsArray ( arr ) {
@@ -35,12 +30,12 @@ function addPhraseToDisplay( arr ) {
     for ( let i = 0; i < arr.length; i++ ) {
         const listItem = document.createElement('li');
         listItem.textContent = arr[i];
+        phraseUL.append(listItem);
         if ( listItem.textContent !== ' ' ) {
             listItem.className = 'letter';
         } else {
             listItem.className = 'space';
         }
-        phraseUL.appendChild(listItem);
     }
 }
 
@@ -66,13 +61,15 @@ function checkWin() {
     if ( letter.length === show.length ) {
         overlay.className += ' win';
         overlay.style.display = 'flex';
-        title.textContent = "You've Won the Game!";
+        subTitle.textContent = "We have a winner!";
         scoreboard.style.display = 'none';
+        startBtn.textContent = 'Try Again';
     } else if ( missed >= 5 ) {
         overlay.className += ' lose';
         overlay.style.display = 'flex';
-        title.textContent = "You've Lost the Game :(";
+        subTitle.textContent = `Sorry, the correct answer was ${phraseArray.join('')}`;
         scoreboard.style.display = 'none';
+        startBtn.textContent = 'Try Again';
     }
 }
 
@@ -90,9 +87,47 @@ keyBoard.addEventListener( 'click', (e) => {
     // keeps track of tries remaining
     if ( !letterFound && letterBtn.tagName === 'BUTTON' ) {
         missed++;
-        const hearts = document.querySelectorAll('.tries')
         hearts[missed - 1].style.opacity = '0.25';
     }
     
     checkWin();
+});
+
+// resets hearts
+function resetHearts() {
+    missed = 0;
+    for ( let i = 0; i < hearts.length; i++ ) {
+        hearts[i].style.opacity = '1';
+        scoreboard.style.display = '';
+    }
+}
+
+// resets keyboard
+function resetKeyboard() {
+    const chosen = document.querySelectorAll('.chosen');
+    for ( let i = 0; i < chosen.length; i++ ) {
+        chosen[i].className = '';
+        chosen[i].disabled = false;
+    }
+}
+
+// resets random phrase
+function resetPhrase() {
+    phraseUL.innerHTML = '';
+    addPhraseToDisplay( getRandomPhraseAsArray( phrases ) )
+}
+
+// resets the game
+function resetGame() {
+    resetKeyboard();
+    resetHearts();
+    resetPhrase();
+    overlay.style.display = 'none';
+}
+
+// Listen for the start / reset game button to be pressed
+startBtn.addEventListener( 'click', (e) => {
+    e.preventDefault();
+    overlay.style.display = 'none';
+    resetGame();
 });
